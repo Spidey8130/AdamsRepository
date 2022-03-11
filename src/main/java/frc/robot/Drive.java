6a5e2fa7a587aa0c3c66976bd098f61b90b37d4f
Spidey8130 <pokemaster0425@gmail.com>
2022-03-11 15:07:38 -0600

    Update Drive.java

--- src/main/java/frc/robot/Drive.java
+++ src/main/java/frc/robot/Drive.java
@@ -1,7 +1,6 @@
 package frc.robot;
 
 //Imports
-// import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.drive.MecanumDrive;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
@@ -11,10 +10,11 @@ import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.PneumaticsControlModule;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
-// import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
-
+import com.kauailabs.navx.frc.AHRS;
+import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.Timer;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.revrobotics.CANSparkMax;
@@ -23,14 +23,15 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 
 public class Drive extends TimedRobot {
+    AHRS navX = new AHRS(SPI.Port.kMXP);
     //Intializing Left Motor
-    public CANSparkMax lFMotor = new CANSparkMax(3, CANSparkMaxLowLevel.MotorType.kBrushless);
-    public CANSparkMax lBMotor = new CANSparkMax(1, CANSparkMaxLowLevel.MotorType.kBrushless);
+    public CANSparkMax lFMotor = new CANSparkMax(2, CANSparkMaxLowLevel.MotorType.kBrushless);
+    public CANSparkMax lBMotor = new CANSparkMax(4, CANSparkMaxLowLevel.MotorType.kBrushless);
     // private MotorControllerGroup lMotors = new MotorControllerGroup(lFMotor, lBMotor);
 
     //Initializing Right Motors
-    public CANSparkMax rFMotor = new CANSparkMax(4, CANSparkMaxLowLevel.MotorType.kBrushless);
-    public CANSparkMax rBMotor = new CANSparkMax(8, CANSparkMaxLowLevel.MotorType.kBrushless);
+    public CANSparkMax rFMotor = new CANSparkMax(1, CANSparkMaxLowLevel.MotorType.kBrushless);
+    public CANSparkMax rBMotor = new CANSparkMax(3, CANSparkMaxLowLevel.MotorType.kBrushless);
     public TalonSRX climber = new TalonSRX(9);
     public MotorControllerGroup rMotors = new MotorControllerGroup(rFMotor, rBMotor);
     public MotorControllerGroup lMotors = new MotorControllerGroup(lFMotor, lBMotor);
@@ -44,8 +45,8 @@ public class Drive extends TimedRobot {
     // private Solenoid rWSolenoid = new Solenoid(PneumaticsModuleType.CTREPCM, 3);
     // private Solenoid lWSolenoid = new Solenoid(PneumaticsModuleType.CTREPCM, 4);
 
-    boolean tank = true;
-    boolean down = false;
+    boolean tank;
+    boolean down;
     double climberM = 0.0;
     //Initializing NonMotors
     MecanumDrive mDrive = new MecanumDrive(lFMotor, lBMotor, rFMotor, rBMotor);
@@ -73,20 +74,29 @@ public class Drive extends TimedRobot {
             down = false;
         }
         if (tank) {
-            tDrive.arcadeDrive(Xbox1.getRightX(), -Xbox1.getLeftY());
+            tDrive.arcadeDrive(-Xbox1.getLeftY(), Xbox1.getRightX());
             climber.set(ControlMode.PercentOutput, climberM);
             //Puts Power of Each Motor Into Smart Dashboard
             SmartDashboard.putNumber("L Power", lMotors.get());
             SmartDashboard.putNumber("R Power", rMotors.get());
         }
         else if (!tank) {
-            mDrive.driveCartesian(Xbox1.getLeftY(), Xbox1.getLeftX(), Xbox1.getRightX());
+            mDrive.driveCartesian(-Xbox1.getLeftY(), Xbox1.getLeftX(), Xbox1.getRightX());
 
             //Puts Power of Each Motor Into Smart Dashboard
             SmartDashboard.putNumber("LF Power", lFMotor.get());
             SmartDashboard.putNumber("LB Power", lBMotor.get());
             SmartDashboard.putNumber("RF Power", rFMotor.get());
             SmartDashboard.putNumber("RB Power", rBMotor.get());
+        } else if (!tank) {
+            mDrive.setSafetyEnabled(true);
+            while (isTeleop() && isEnabled()) {
+                if ( Xbox1.getRightBumperPressed()) {
+                    navX.reset();
+                }
+                mDrive.driveCartesian(Xbox1.getLeftX(), Xbox1.getLeftY(), Xbox1.getRightX(), navX.getAngle());
+                Timer.delay(0.005);
+            }
         }
         
     }
